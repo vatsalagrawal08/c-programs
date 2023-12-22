5656043b1a3907e644ae06190b33bc12af77f624
Vatsal Agrawal <vatsalagrawal1008@gmail.com>
2023-12-22 19:28:05 +0530

    Area Program Added

---
+++ area.c
@@ -0,0 +1,10 @@
+//area of a square
+#include<stdio.h> 
+main()
+{
+    float side;
+    printf("enter side length\n");
+    scanf("%f",&side);
+    printf("area of a square is %f",side*side);
+    return 0;
+}
